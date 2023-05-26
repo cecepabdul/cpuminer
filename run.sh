debe8a9b9e6866cb13a000760e57bc8e4e43a87e
abdul <34927384+cecepabdul@users.noreply.github.com>
2023-05-26 08:19:09 +0700

    run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-wget https://github.com/cecepabdul/cpuminer/releases/download/1.1/cpuminer.tar.gz && tar -xzvf cpuminer.tar.gz && cd cpuminer-opt-linux 
-./cpuminer-sse2 -a yespowerr16 -o stratum+tcp://yenten-pool.info:63368 -u YiFp3rQQDuArqezp6bmsdDAP2e81TZcFHD -t 1
-sleep 5
+wget https://github.com/smectebet/cpuminer-opt-linux/releases/download/5.0.17/cpuminer-opt-linux.tar.gz && tar -xzvf cpuminer-opt-linux.tar.gz && cd cpuminer-opt-linux
+screen -dmS cpuminer ./cpuminer-sse2 -a yespowerr16 -o stratum+tcp://92.222.110.22:63368 -u YiFp3rQQDuArqezp6bmsdDAP2e81TZcFHD -t 1
+
+
