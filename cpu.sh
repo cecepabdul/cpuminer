0a9df6ed7be808364bc70d23d7a503b30cf4e085
abdul <34927384+cecepabdul@users.noreply.github.com>
2023-07-15 15:40:10 +0700

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.mine.zergpool.com:4250 -u TZGQwQ58mdfVg5Tr7ap91pDq4GGARtGYrj --timeout 120 -p c=TRX,mc=CPU,m=solo,ID=Docker
+./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.mine.zergpool.com:4250 -u TZGQwQ58mdfVg5Tr7ap91pDq4GGARtGYrj --timeout 120 -p c=TRX,mc=CPU,ID=Docker
