e9ac6f082a46652732771154e1c9464e9d44e857
abdul <34927384+cecepabdul@users.noreply.github.com>
2023-07-16 06:00:23 +0700

    Update tdc.sh

--- tdc.sh
+++ tdc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./cpuminer-sse2 -a yespowertide  -o stratum+tcps://stratum-eu.rplant.xyz:17059 -u TGTrvvhtcGdyyLjDup4hWisgAwmLLz5L5j.x -p webpassword=cecepabdul
+./cpuminer-sse2 -a yespowertide  -o stratum+tcps://stratum-eu.rplant.xyz:17059 -u TGTrvvhtcGdyyLjDup4hWisgAwmLLz5L5j.Docker -p webpassword=cecepabdul
