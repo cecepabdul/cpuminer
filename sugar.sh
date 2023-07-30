117d12919511cba61515ef70959add42fb894984
abdul <34927384+cecepabdul@users.noreply.github.com>
2023-07-30 07:46:53 +0700

    Update sugar.sh

--- sugar.sh
+++ sugar.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./cpuminer-sse2 -a yespowersugar  -o stratum+tcp://stratum-na.rplant.xyz:7042 -u sugar1q4vcxkt82achnuqzwsavgha6jyurtgywj26grpj.d -p webpassword=cecepabdul
+./cpuminer-sse2 -a yespowersugar  -o stratum+tcp://stratum-na.rplant.xyz:7042 -u sugar1q4vcxkt82achnuqzwsavgha6jyurtgywj26grpj.dt -p webpassword=cecepabdul
