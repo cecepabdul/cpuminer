9d77538126439f0a36ce5d7c70f92abd3170f406
abdul <34927384+cecepabdul@users.noreply.github.com>
2023-08-07 14:23:50 +0700

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./cpuminer-sse2 -a minotaurx  -o stratum+tcps://stratum-eu.rplant.xyz:17068 -u RFP5QbYEoYYd7vm7A6reZNYaZ4vLcMovS2.x -p webpassword=cecepabdul
+./cpuminer-sse2 -a minotaurx  -o stratum+tcps://stratum-eu.rplant.xyz:17068 -u RFP5QbYEoYYd7vm7A6reZNYaZ4vLcMovS2.d -p webpassword=cecepabdul
